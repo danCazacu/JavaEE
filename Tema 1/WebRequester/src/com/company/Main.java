0691776ae4c1a08f86edb80a601e9b3dd8400804
Dan Cazacu <dan.cazacu@softvision.ro>
2018-10-09 11:11:02 +0300

    Improved WebRequester

--- Tema 1/WebRequester/src/com/company/Main.java
+++ Tema 1/WebRequester/src/com/company/Main.java
@@ -1,14 +1,32 @@
 package com.company;
 
+import java.io.*;
+import java.util.ArrayList;
 import java.util.concurrent.CountDownLatch;
 
 public class Main {
 
     public static void main(String[] args) {
+
+        /**
+         * get the name list
+         */
+
+        ArrayList<String> lstNames = null;
+        try {
+
+            lstNames = readFromFile_getNamesList();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+
         CountDownLatch latch = new CountDownLatch(1);
-        MyThread threads[] = new MyThread[500];
+        MyThread threads[] = new MyThread[1000];
         for(int i = 0; i<threads.length; i++){
-            threads[i] = new MyThread(latch);
+
+            threads[i] = new MyThread(latch, lstNames);
         }
         for(int i = 0; i<threads.length; i++){
             threads[i].start();
@@ -29,13 +47,50 @@ public class Main {
             e.printStackTrace();
         }
         latch = new CountDownLatch(1);
-        MyThread threads2[] = new MyThread[500];
+        MyThread threads2[] = new MyThread[1000];
         for(int i = 0; i<threads2.length; i++){
-            threads2[i] = new MyThread(latch);
+            threads2[i] = new MyThread(latch, lstNames);
         }
         for(int i = 0; i<threads2.length; i++){
             threads2[i].start();
         }
         latch.countDown();
     }
+
+    public static ArrayList<String> readFromFile_getNamesList() throws IOException{
+
+        ArrayList<String> lstNames = new ArrayList<>();
+
+        FileReader fr = null;
+        BufferedReader br = null;
+        try {
+            fr = new FileReader(System.getProperty("user.home") + "/Desktop/JavaEE_Lab1/names.txt");
+            br = new BufferedReader(fr);
+
+        } catch (FileNotFoundException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+
+        String line;
+        int lineCount = 1;
+        while((line = br.readLine()) != null){
+
+            if (!line.equals("") && !line.startsWith("https"))
+            {
+
+                lstNames.add(line.replaceAll(" ", ""));
+            }
+        }
+
+        fr.close();
+
+        return lstNames;
+    }
+
+
+
+
 }
--- Tema 1/WebRequester/src/com/company/MyThread.java
+++ Tema 1/WebRequester/src/com/company/MyThread.java
@@ -5,13 +5,21 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.net.HttpURLConnection;
 import java.net.URL;
+import java.util.ArrayList;
+import java.util.Random;
 import java.util.concurrent.CountDownLatch;
 
 public class MyThread extends Thread {
+
     CountDownLatch latch;
-    public MyThread(CountDownLatch latch) {
+    ArrayList<String> namesList;
+
+    public MyThread(CountDownLatch latch, ArrayList<String> names) {
+
         this.latch = latch;
+        this.namesList = names;
     }
+
     @Override
     public void run() {
         try
@@ -23,11 +31,28 @@ public class MyThread extends Thread {
         request();
 
     }
+
+
     public void request()
     {
         URL url = null;
         try {
-            url = new URL("http://localhost:8080/Java_Lab1/Servlet1?name=dfgdfg&email=ghfghfh&submit=javaapp");
+
+            String strURL ;
+            if(namesList.size() > 0){
+
+                Random random = new Random();
+                int randomNumber = random.nextInt(namesList.size());
+                String name = namesList.get(randomNumber);
+                String email = name + "@javalab1.com";
+
+                strURL = "http://localhost:8080/Java_Lab1/Servlet1?name=" + name + "&email=" + email + "&submit=javaapp";
+            }else{
+
+                strURL = "http://localhost:8080/Java_Lab1/Servlet1?name=" + "someName" + "&email=" + "someName@javalab1.com" + "&submit=javaapp";
+            }
+
+            url = new URL(strURL);
             HttpURLConnection con = (HttpURLConnection) url.openConnection();
             con.setRequestMethod("GET");
             long timeBeforeRequest = System.currentTimeMillis();
@@ -43,6 +68,7 @@ public class MyThread extends Thread {
 
             System.out.println(""+(timeAfterRequest-timeBeforeRequest)+" "+response);
         } catch (IOException e) {
+
             e.printStackTrace();
         }
     }
