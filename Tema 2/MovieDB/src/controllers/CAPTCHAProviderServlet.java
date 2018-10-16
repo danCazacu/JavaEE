0dbe8668752f5f31b27ec52f494f9dcb5418d964
AndreeaProdan <andreeaprodan31@gmail.com>
2018-10-16 12:46:57 +0300

    Captcha colored

--- Tema 2/MovieDB/src/controllers/CAPTCHAProviderServlet.java
+++ Tema 2/MovieDB/src/controllers/CAPTCHAProviderServlet.java
@@ -7,9 +7,11 @@ import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.awt.*;
+import java.awt.font.TextAttribute;
 import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.text.AttributedString;
 import java.util.Random;
 
 @WebServlet(name = "CAPTCHAProviderServlet", urlPatterns = "/CAPTCHAProviderServlet")
@@ -24,17 +26,48 @@ public class CAPTCHAProviderServlet extends HttpServlet {
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         StringBuilder s = new StringBuilder();
         Random random = new Random();
-        for(int i=0;i<4;i++){
-            s.append(upper.charAt(random.nextInt(upper.length())));
+        for(int i=0;i < 4; i++){
+
+            if(random.nextInt(100) > 50) {
+                s.append(upper.charAt(random.nextInt(upper.length())));
+            }else if(random.nextInt(100) < 50){
+
+                s.append(upper.toLowerCase().charAt(random.nextInt(upper.length())));
+            }else{
+
+                s.append(random.nextInt(10));
+            }
         }
         int width = 100;
-        int height = 25;
+        int height = 50;
         BufferedImage bufferedImage = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
         Graphics2D g2d = bufferedImage.createGraphics();
         g2d.setColor(Color.black);
+
+        Random r = new Random();
+        Color randomColor0 = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat()); //rgb
+        Color randomColor1 = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat()); //rgb
+        Color randomColor2 = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat()); //rgb
+        Color randomColor3 = new Color(r.nextFloat(), r.nextFloat(), r.nextFloat()); //rgb
+
+        Font font1 = new Font("Serif", Font.ITALIC, 12);
+        Font font2 = new Font("Monospaced", Font.BOLD, 20);
+        Font font3 = new Font("Courier", Font.ITALIC+Font.BOLD, 19);
+        Font font4 = new Font("ZapfDingbats", Font.CENTER_BASELINE, 15);
+
+
+        AttributedString as = new AttributedString(s.toString());
+        as.addAttribute(TextAttribute.FONT, font1);
+        as.addAttribute(TextAttribute.FONT, font2, 1, 2);
+        as.addAttribute(TextAttribute.FONT, font3, 2, 3);
+        as.addAttribute(TextAttribute.FONT, font4, 3, 4);
+        as.addAttribute(TextAttribute.FOREGROUND, randomColor0, 0, 1);
+        as.addAttribute(TextAttribute.FOREGROUND, randomColor1, 1, 2);
+        as.addAttribute(TextAttribute.FOREGROUND, randomColor2, 2, 3);
+        as.addAttribute(TextAttribute.FOREGROUND, randomColor3, 3, 4);
+
         g2d.fillRect(0, 0, width, height);
-        g2d.setColor(Color.white);
-        g2d.drawString(s.toString(), 50, 10);
+        g2d.drawString(as.getIterator(), 50, 10);
         g2d.dispose();
 
         ImageIO.write(bufferedImage, "png", response.getOutputStream());
