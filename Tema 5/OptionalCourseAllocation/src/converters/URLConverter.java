8603867f9689762925a6800957a888e928c7c3bf
Andreea Prodan <andreeaprodan31@gmail.com>
2018-11-06 14:12:50 +0200

    new

---
+++ Tema 5/OptionalCourseAllocation/src/converters/URLConverter.java
@@ -0,0 +1,40 @@
+package converters;
+
+import javax.faces.application.FacesMessage;
+import javax.faces.component.UIComponent;
+import javax.faces.context.FacesContext;
+import javax.faces.convert.Converter;
+import javax.faces.convert.ConverterException;
+import javax.faces.convert.FacesConverter;
+import java.net.URL;
+
+@FacesConverter(value="urlConverter")
+public class URLConverter implements Converter {
+
+    @Override
+    public Object getAsObject(FacesContext facesContext, UIComponent uiComponent, String newValue) {
+        try{
+
+           /* if(!newValue.startsWith("http://")){
+
+                newValue = "http://" + newValue;
+            }
+*/
+            return new URL(newValue);
+        }catch(Exception e){
+
+            FacesMessage msg =
+                    new FacesMessage("URL Conversion error.",
+                            "Invalid URL format.");
+            msg.setSeverity(FacesMessage.SEVERITY_ERROR);
+
+            throw new ConverterException( msg);
+
+        }
+    }
+
+    @Override
+    public String getAsString(FacesContext facesContext, UIComponent uiComponent, Object value) {
+        return String.valueOf(value);
+    }
+}
---
+++ Tema 5/OptionalCourseAllocation/src/gallery/GalleryView.java
@@ -0,0 +1,28 @@
+package gallery;
+
+import javax.annotation.PostConstruct;
+import javax.faces.bean.ManagedBean;
+import javax.faces.context.FacesContext;
+import java.util.ArrayList;
+import java.util.List;
+
+@ManagedBean
+public class GalleryView {
+
+    private List<String> images;
+
+    @PostConstruct
+    public void init() {
+
+        FacesContext.getCurrentInstance().getExternalContext().getSession(true);
+
+        images = new ArrayList<String>();
+        for (int i = 1; i < 6; i++) {
+            images.add("imageCS" + i + ".jpg");
+        }
+    }
+
+    public List<String> getImages() {
+        return images;
+    }
+}
